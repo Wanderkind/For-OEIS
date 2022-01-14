cccaeadeafd26fd81eabfc91d002021af9fa8a7d
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-01-14 14:42:28 +0900

    Add files via upload

---
+++ 4dseq.py
@@ -0,0 +1,24 @@
+
+R = 0
+
+while True:
+    Count = 0
+
+    for I in range(2*R + 1):
+        i = I - R
+
+        for J in range(2*R + 1):
+            j = J - R
+
+            for K in range(2*R + 1):
+                k = K - R
+
+                for L in range(2*R + 1):
+                    l = L - R
+                    
+                    
+                    if R == 0 or (2*R - 1)**2 <= 4*(i**2 + j**2 + k**2 + l**2) <= (2*R + 1)**2:
+                        Count += 1
+
+    print(Count)
+    R += 1
